9c6ca9c2e62e43ba669b43976c97dd7787a8f860
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-12 17:50:16 +0000

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
--- Task: List all bands with Glam rock as their main style, ranked by longevity
-
--- Calculate lifespan based on the 'formed' and 'split' attributes
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+-- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
