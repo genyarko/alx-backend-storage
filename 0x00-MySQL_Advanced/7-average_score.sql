01147594e84de7df2589541cddcc709b41d59fdb
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-12 17:59:40 +0000

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,26 +1,22 @@
 -- Task: Create a stored procedure to compute and store the average score for a student
-
-DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10, 2);
-    
-    -- Calculate the average score for the user
-    SELECT AVG(score) INTO avg_score
-    FROM student_scores
-    WHERE student_id = user_id;
-    
-    -- Check if a record already exists for the user
-    IF EXISTS (SELECT 1 FROM user_average_scores WHERE user_id = user_id) THEN
-        -- Update the existing record with the new average score
-        UPDATE user_average_scores
-        SET average_score = avg_score
-        WHERE user_id = user_id;
-    ELSE
-        -- Insert a new record with the average score
-        INSERT INTO user_average_scores (user_id, average_score)
-        VALUES (user_id, avg_score);
-    END IF;
-END;
-//
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
+
+    SELECT SUM(score)
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+
+    UPDATE users
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        WHERE users.id = user_id;
+END $$
 DELIMITER ;
