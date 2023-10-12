715fa624066f0d5729ff7f997e7e7698052adeff
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-12 17:48:13 +0000

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,5 @@
--- Task: Rank country origins of bands by the number of (non-unique) fans
-
--- Rank countries by the number of fans in descending order
-SELECT origin, SUM(nb_fans) as nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+-- Ranks country origins of bands, ordered by the number of (non-unique) fans.
+SELECT origin, SUM(fans) AS nb_fans
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
