2cf3e4123a5dcecd04871110b67aac650fca218d
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-12 17:40:00 +0000

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,11 +1,19 @@
-/*
- * Create table 'users' if it doesn't exist
- */
+-- Task: Create table 'users' with specified attributes and constraints
 
+-- Create table 'users' if it doesn't exist
 CREATE TABLE IF NOT EXISTS users (
+    -- Define 'id' as an auto-incremented primary key
     id INT NOT NULL AUTO_INCREMENT,
+    
+    -- Define 'email' as a unique, not null column
     email VARCHAR(255) NOT NULL,
+    
+    -- Define 'name' as a VARCHAR(255) column
     name VARCHAR(255),
+    
+    -- Set 'id' as the primary key
     PRIMARY KEY (id),
+    
+    -- Enforce uniqueness on 'email' column
     UNIQUE (email)
 );
