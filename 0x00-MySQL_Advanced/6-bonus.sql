64fbd2780ff6e16636f0b073f13912f24276bfee
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-12 17:56:55 +0000

    Update 6-bonus.sql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,17 +1,24 @@
-DELIMITER //
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+-- SQL script that creates a stored procedure AddBonus that adds a new correction for a student
+DROP PROCEDURE IF EXISTS AddBonus;
+DELIMITER $$
+CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
 BEGIN
-    DECLARE project_id INT;
-    
-    -- Check if the project exists, and if not, create it
-    SELECT id INTO project_id FROM projects WHERE name = project_name;
-    IF project_id IS NULL THEN
-        INSERT INTO projects (name) VALUES (project_name);
-        SET project_id = LAST_INSERT_ID();
+    DECLARE project_count INT DEFAULT 0;
+    DECLARE project_id INT DEFAULT 0;
+
+    SELECT COUNT(id)
+        INTO project_count
+        FROM projects
+        WHERE name = project_name;
+    IF project_count = 0 THEN
+        INSERT INTO projects(name)
+            VALUES(project_name);
     END IF;
-    
-    -- Insert the correction
-    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-END;
-//
+    SELECT id
+        INTO project_id
+        FROM projects
+        WHERE name = project_name;
+    INSERT INTO corrections(user_id, project_id, score)
+        VALUES (user_id, project_id, score);
+END $$
 DELIMITER ;
