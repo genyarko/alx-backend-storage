54d6c0dc71f37ca1d8bad193db85dfae828326a5
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-18 15:49:00 +0000

    Create exercise.py

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,28 @@
+import redis
+import uuid
+from typing import Union
+
+class Cache:
+    """
+    Cache class for storing data in Redis with random keys.
+    """
+    def __init__(self):
+        """
+        Initialize the Cache class with a Redis client and flush the Redis database.
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in Redis with a randomly generated key.
+
+        Args:
+            data (Union[str, bytes, int, float]): The data to be stored.
+
+        Returns:
+            str: The randomly generated key used for storing the data in Redis.
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
