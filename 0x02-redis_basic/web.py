209d941548ca05a9ae6f53172480295643ad6683
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-18 16:08:38 +0000

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python3
 '''A module with tools for request caching and tracking.
 '''
-import requests
 import redis
-import time
+import requests
+from functools import wraps
+from typing import Callable
 
+# Initialize a module-level Redis instance
+redis_store = redis.Redis()
+
+def data_cacher(method: Callable) -> Callable:
+    '''Decorator that caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''Wrapper function for caching the output.
+        '''
+        # Increment the count for the URL
+        redis_store.incr(f'count:{url}')
+        
+        # Try to retrieve the cached result
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        
+        # If the result is not cached, fetch it and cache it with a 10-second expiration
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)  # Reset the count to 0
+        redis_store.setex(f'result:{url}', 10, result)  # Cache the result
+        return result
+    return invoker
+
+@data_cacher
 def get_page(url: str) -> str:
-    # Initialize a Redis client
-    r = redis.Redis()
-
-    # Key to track the count of URL access
-    count_key = f'count:{url}'
-
-    # Get the count from the cache or set it to 0 if not exists
-    count = int(r.get(count_key) or 0)
-
-    # Increment the count
-    count += 1
-
-    # Store the updated count with a 10-second expiration time
-    r.setex(count_key, 10, count)
-
-    # Get the HTML content from the URL
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        return response.text
-    else:
-        return f"Failed to retrieve the page from {url}"
-
-# Example usage:
-if __name__ == '__main__':
-    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/text"
-    html_content = get_page(url)
-    print(f"HTML content from {url}:\n{html_content}")
+    '''Function that returns the content of a URL after caching the request's response and tracking the request.
+    '''
+    return requests.get(url).text
