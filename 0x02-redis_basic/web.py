1e9bcc6dc9966841d2da0e8a459bf8d8e832a0c3
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-18 16:18:40 +0000

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,94 +1,36 @@
 #!/usr/bin/env python3
 '''A module with tools for request caching and tracking.
 '''
-import redis
 import requests
-from functools import wraps
-from typing import Callable
+import redis
+import time
 
+def get_page(url: str) -> str:
+    # Initialize a Redis client
+    r = redis.Redis()
 
-class RedisStore:
-    def __init__(self, host: str = "localhost", port: int = 6379):
-        self.client = redis.Redis(host=host, port=port)
+    # Key to track the count of URL access
+    count_key = f'count:{url}'
 
-    def incr(self, key: str) -> int:
-        return self.client.incr(key)
+    # Get the count from the cache or set it to 0 if not exists
+    count = int(r.get(count_key) or 0)
 
-    def get(self, key: str) -> bytes:
-        return self.client.get(key)
+    # Increment the count
+    count += 1
 
-    def set(self, key: str, value: bytes):
-        self.client.set(key, value)
+    # Store the updated count with a 10-second expiration time
+    r.setex(count_key, 10, count)
 
-    def setex(self, key: str, ttl: int, value: bytes):
-        self.client.setex(key, ttl, value)
+    # Get the HTML content from the URL
+    response = requests.get(url)
 
-
-def get_redis_store() -> RedisStore:
-    """Returns a Redis store instance."""
-    return RedisStore()
-
-
-def data_cacher(cache_key: str, cache_ttl: int) -> Callable:
-    """A decorator for caching the results of a function.
-
-    Args:
-        cache_key: The cache key to use for the function.
-        cache_ttl: The expiration time for cached items in seconds.
-
-    Returns:
-        A decorator function.
-    """
-
-    def decorator(func: Callable) -> Callable:
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            """The wrapper function for caching the output."""
-            redis_store = get_redis_store()
-
-            redis_store.incr(f'count:{cache_key}')
-            result = redis_store.get(f'result:{cache_key}')
-            if result:
-                return result.decode('utf-8')
-
-            result = func(*args, **kwargs)
-            redis_store.set(f'count:{cache_key}', 0)
-            redis_store.setex(f'result:{cache_key}', cache_ttl, result)
-            return result
-
-        return wrapper
-
-    return decorator
-
-
-@data_cacher(cache_key='get_page', cache_ttl=10)
-def get_page(url: str, timeout: int = 5, cache_buster: bool = False) -> str:
-    """Returns the content of a URL after caching the request's response,
-    and tracking the request.
-
-    Args:
-        url: The URL to fetch.
-        timeout: The maximum amount of time to wait for a response from the
-            server before returning an error.
-        cache_buster: Whether to bypass the cache and fetch the latest data from
-            the server.
-
-    Returns:
-        The content of the URL.
-    """
-
-    if cache_buster:
-        redis_store = get_redis_store()
-        redis_store.delete(f'result:{cache_key}')
-
-    response = requests.get(url, timeout=timeout)
-    return response.text
-
+    if response.status_code == 200:
+        return response.text
+    else:
+        return f"Failed to retrieve the page from {url}"
 
 if __name__ == '__main__':
-    url = "https://example.com"
-
-    # Fetch the page content from the cache, or from the server if the cache is not hit.
-    page_content = get_page(url)
-
-    print(page_content)
+    # Example usage:
+    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/text"
+    html_content = get_page(url)
+    print(f"HTML content from {url}:\n{html_content}")
