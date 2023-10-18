0225a0be713ae17eb445deb15ed407111adaca08
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-18 15:51:11 +0000

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 class Cache:
     """
@@ -26,3 +26,45 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """
+        Retrieve data from Redis and optionally apply a conversion function.
+
+        Args:
+            key (str): The key used to retrieve data from Redis.
+            fn (Callable, optional): A callable function to convert the data. Defaults to None.
+
+        Returns:
+            Union[str, bytes, int, float]: The retrieved data, possibly converted by the provided function.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieve a string from Redis.
+
+        Args:
+            key (str): The key used to retrieve data from Redis.
+
+        Returns:
+            str: The retrieved string.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieve an integer from Redis.
+
+        Args:
+            key (str): The key used to retrieve data from Redis.
+
+        Returns:
+            int: The retrieved integer.
+        """
+        return self.get(key, fn=int)
