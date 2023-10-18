06bebb4767cfbb272aa2701acfcc194af3cc814f
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-18 16:04:36 +0000

    Create web.py

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+'''A module with tools for request caching and tracking.
+'''
+import requests
+import redis
+import time
+
+def get_page(url: str) -> str:
+    # Initialize a Redis client
+    r = redis.Redis()
+
+    # Key to track the count of URL access
+    count_key = f'count:{url}'
+
+    # Get the count from the cache or set it to 0 if not exists
+    count = int(r.get(count_key) or 0)
+
+    # Increment the count
+    count += 1
+
+    # Store the updated count with a 10-second expiration time
+    r.setex(count_key, 10, count)
+
+    # Get the HTML content from the URL
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.text
+    else:
+        return f"Failed to retrieve the page from {url}"
+
+# Example usage:
+if __name__ == '__main__':
+    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/text"
+    html_content = get_page(url)
+    print(f"HTML content from {url}:\n{html_content}")
