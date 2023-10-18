5f77eca4f8295c5dff4da52e15c42a9987c89581
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-18 16:01:11 +0000

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -36,33 +36,28 @@ def call_history(method: Callable) -> Callable:
     return invoker
 
 def replay(fn: Callable) -> None:
-    '''Displays the call history of a Cache class's method.
+    '''Displays the call history of a Cache class' method.
     '''
     if fn is None or not hasattr(fn, '__self__'):
         return
-    
     redis_store = getattr(fn.__self__, '_redis', None)
-    
     if not isinstance(redis_store, redis.Redis):
         return
-    
-    method_name = fn.__qualname__
-    in_key = '{}:inputs'.format(method_name)
-    out_key = '{}:outputs'.format(method_name)
-    call_count = 0
-    
-    if redis_store.exists(method_name) != 0:
-        call_count = int(redis_store.get(method_name))
-    
-    print('{} was called {} times:'.format(method_name, call_count))
-    
-    inputs = redis_store.lrange(in_key, 0, -1)
-    outputs = redis_store.lrange(out_key, 0, -1)
-    
-    for input_data, output_data in zip(inputs, outputs):
-        input_str = ', '.join(input_data.decode("utf-8").split(', '))
-        output_str = output_data.decode("utf-8")
-        print('{}(*({})) -> {}'.format(method_name, input_str, output_str)
+    fxn_name = fn.__qualname__
+    in_key = '{}:inputs'.format(fxn_name)
+    out_key = '{}:outputs'.format(fxn_name)
+    fxn_call_count = 0
+    if redis_store.exists(fxn_name) != 0:
+        fxn_call_count = int(redis_store.get(fxn_name))
+    print('{} was called {} times:'.format(fxn_name, fxn_call_count))
+    fxn_inputs = redis_store.lrange(in_key, 0, -1)
+    fxn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
+        print('{}(*{}) -> {}'.format(
+            fxn_name,
+            fxn_input.decode("utf-8"),
+            fxn_output,
+        ))
 
 class Cache:
     '''Represents an object for storing data in a Redis data storage.
@@ -101,10 +96,3 @@ class Cache:
         '''Retrieves an integer value from a Redis data storage.
         '''
         return self.get(key, lambda x: int(x))
-
-# Example usage:
-cache = Cache()
-cache.store("foo")
-cache.store("bar")
-cache.store(42)
-replay(cache.store)
